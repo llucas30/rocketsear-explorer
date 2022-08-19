cb4879760408c7e331174a6b3a9ba9863bb43c5d
Lucas Curvello <91077554+llucas30@users.noreply.github.com>
2022-08-19 18:24:53 -0300

    Add files via upload

---
+++ ll2/js/index.js
@@ -0,0 +1,19 @@
+// variáveis - variables
+const form = document.querySelector('form')
+const inputWeight = document.querySelector('#weight')
+const inputHeight = document.querySelector('#height')
+
+// 3 maneiras de criar e atribuir função a um evento
+// form.onsubmit = function () {}
+
+// form.onsubmit = () => {}
+
+// form.onsubmit = handlesubmit
+
+// function handlesubmit () {
+
+// }
+
+form.onsubmit = function() {
+    console.log
+}
